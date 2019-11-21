cd651ebe683763501d36b65b6682ce24a1f8c3d2
Chris <louischrisorona@gmail.com>
2019-11-20 17:03:41 -0800

    Initial commit

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-var express        = require("express"),
-    path           = require("path"),
-    app            = express(),
-    bodyParser     = require("body-parser"),
-    mongoose       = require("mongoose"),
-    Campground     = require("./models/campground"),
-    Comment        = require("./models/comment"),
-    localStrategy  = require("passport-local"),
-    passport       = require("passport"),
-    User           = require("./models/user"),
+const express = require("express"),
+    path = require("path"),
+    app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    Campground = require("./models/campground"),
+    Comment = require("./models/comment"),
+    localStrategy = require("passport-local"),
+    passport = require("passport"),
+    User = require("./models/user"),
     methodOverride = require("method-override"),
-    flash          = require("connect-flash"),
-    seedDB         = require("./seeds");
+    flash = require("connect-flash"),
+    seedDB = require("./seeds");
 
 //requiring routes    
 var campgroundRoutes = require("./routes/campgrounds"),
     commentRoutes    = require("./routes/comments"),
     authRoutes       = require("./routes/index");
     
-mongoose.connect(process.env.DATABASEURL);
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp_v14";
+mongoose.connect(url);
 // mongoose.connect("mongodb://Chris:Sirs5s3r!@ds127993.mlab.com:27993/yelpcamp_tfp33184");
-console.log();
 
 mongoose.Promise = global.Promise;
 app.use(bodyParser.urlencoded({extended: true}));
